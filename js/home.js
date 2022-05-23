fbf6e00f81c9ae6e60d820f5bf151d78bf4b50fe
Lee-seungju <seungju0000@naver.com>
2022-05-24 00:34:46 +0900

    3/5 2.popup

--- js/home.js
+++ js/home.js
@@ -7,3 +7,10 @@ function moveDetail(id) {
 	var content = encodeURI(encodeURIComponent(document.getElementById(id+"content").value));
 	location.href="detail.html?title=" + title + "&content=" + content;
 }
+
+function showPopup(id) {
+	var title = encodeURI(encodeURIComponent(document.getElementById(id).value));
+
+	console.log("aaa");
+	window.open("title.html?title=" + title, "popup", "width=400,height=300,left=100,top=50");
+}
